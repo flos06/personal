4dcb95e468977dd83c6460cb910a637ef5991e59
flos06 <floriset@gmail.com>
2022-10-04 14:47:07 +0200

    add contact info

--- src/js/loadHome.js
+++ src/js/loadHome.js
@@ -19,13 +19,12 @@ function createHome() {
     Ik ben nu al een tijdje serieus bezig met het online programma The Odin Project en ook met een online vak van de Universiteit van Helsinki over Computer Science en dan met name via de taal Python.
     Tijdens de corona pandemie ben ik serieus begonnen met programmeren en web development omdat ik niet stil wilde zitten. Ik was op dat moment nog in opleiding tot Piloot bij Transavia Airlines.
     Ik heb gemerkt dat ik het programmeren super leuk vindt en wil die zeker naast mijn huidige baan blijven doen. En het liefste zou ik er zelfs carriére in maken.
-    Deze site is op dit moment een ruwe opzet, maar ik verwacht hem in de toekomst uit te gaan breiden.`
+    Deze site is op dit moment een ruwe opzet, maar ik verwacht hem in de toekomst uit te gaan breiden.
+    
+    Mijn github is: https://github.com/flos06. Je kunt contact met mij opnemen via floriset@gmail.com`
 
     home.appendChild(info)
 
-    const contact = document.createElement("div")
-    contact.classList.add("fixed-bottom")
-    contact.textContent = "Mijn github is: https://github.com/flos06. Je kunt contact met mij opnemen via floriset@gmail.com"
     return home;
 }
 
